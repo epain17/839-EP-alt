da0adfd09be4c5c2314d4f439268ba6509dd6285
Emil Petersson <emil.petersson17@yahoo.com>
2017-09-14 13:40:15 +0200

    kommentera kvar

--- 839 EP alt.cpp
+++ 839 EP alt.cpp
@@ -1,11 +1,55 @@
-// 839 EP alt.cpp : Defines the entry point for the console application.
+// 839 EP.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
+#include "iostream"
 
-
+int CalW();
 int main()
 {
-    return 0;
+
+	int nrofCases = 0;
+	int balance;
+
+	scanf_s("%d", &nrofCases);
+	while (nrofCases--)
+	{
+
+		balance = CalW();
+		if (balance == 0)
+		{
+			std::cout << "" << std::endl;
+			std::cout << "YES" << std::endl;
+		}
+
+		else
+		{
+			std::cout << "" << std::endl;
+			std::cout << "NO" << std::endl;
+		}
+
+	}
+
+	return 0;
 }
 
+int CalW()
+{
+	int wl, dl, wr, dr;
+	scanf_s("%d %d %d %d", &wl, &dl, &wr, &dr);
+	if (wl == 0)
+	{
+		wl = CalW();
+	}
+	if (wr == 0)
+	{
+		wr = CalW();
+	}
+
+	return ((wl*dl) - (wr * dr));
+
+
+
+
+
+}
